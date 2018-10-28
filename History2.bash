ed3b660bc3e9cb2b5749b0d89ac6b1b90ca120eb
Lukasz Piat <lukasz.piatkowski@outlook.com>
2018-10-28 14:43:55 +0100

    hist

---
+++ History2.bash
@@ -0,0 +1,458 @@
+    1  python
+    2  python3
+    3  sudo apt install curl https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
+    4  bash Anaconda3-4.3.1-Linux-x86_64.sh
+    5  sudo apt install Anaconda3-4.3.1-Linux-x86_64.sh
+    6  sudo apt-cache https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
+    7  cd/tmp
+    8  cd /tmp
+    9  curl -0 https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
+   10  sudo apt install curl -0 https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
+   11  sudo apt install curl https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
+   12  sudo apt install curl https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
+   13  cd /downloads
+   14  cd /Downloads
+   15  sudo add-apt-repository "deb  https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh"
+   16  bash  https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
+   17  bash:  https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
+   18  sudo apt install <deb https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh>
+   19  sudo apt install <deb https://repo.continuum.io/arc_hive/Anaconda3-4.3.1-Linux-x86_64.sh>
+   20  pwd
+   21  cd Downloads
+   22  w get https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
+   23  bash ./Anaconda3-4.3.1-Linux-x86_64.sh
+   24  anaconda
+   25  sudo
+   26  run anaconda
+   27  conda --version
+   28  run anaconda
+   29  anaconda-navigator
+   30  sudo add-apt-repository ppa:webupd8team/java
+   31  sudo apt-get update
+   32  sudo apt-get install oracle-java8-installer
+   33  conda install -c conda-forge pyspark
+   34  python
+   35  anaconda-navigator
+   36  java8
+   37  openjdk-8-jdk-headless
+   38  jdk
+   39  openjdk-8
+   40  java -verion
+   41  java -version
+   42  java
+   43  jdk8
+   44  jdk
+   45  deb openjdk-8-jdk-headless
+   46  openjdk-8-jdk-headless
+   47  jdk-8
+   48  openkdk-8
+   49  openjdk-8
+   50  jdk-8
+   51  jdk-8-navigator
+   52  jdk-navigator
+   53  intlijiij
+   54  jdk
+   55  df -h
+   56  cd /media/lukasz/VBox_GAs_5.2.18/
+   57  ls
+   58  ls -al
+   59  vf 64Bit/
+   60  cd 64Bit/
+   61  ls
+   62  cat Readme.txt 
+   63  cd ..
+   64  ./runasroot.sh 
+   65  sudo ./runasroot.sh 
+   66  sudo su -
+   67  ls -al
+   68  ls -al /var/log/mes*
+   69  ls -al /var/log/*
+   70  ls -al /var/log/apport.log
+   71  ls -al
+   72  ls
+   73  pwd
+   74  cd /home/lukasz/Downloads/
+   75  ls -al
+   76  cd ../../../
+   77  cd 
+   78  cd $HOME
+   79  pwd
+   80  cd /
+   81  ls
+   82  pwd
+   83  cd 
+   84  pwd
+   85  cd -
+   86  cd $HOME
+   87  pwd
+   88  env
+   89  grep t /etc/passwd
+   90  grep root /etc/passwd
+   91  grep -n root /etc/passwd
+   92  grep -l root /etc/*
+   93  sudo grep -l root /etc/*
+   94  env
+   95  env | grep HOME
+   96  env; grep HOME
+   97  env | grep HOME 
+   98  su -
+   99  cat /etc/sudoers
+  100  sudo cat /etc/sudoers
+  101  sudo cat /etc/sudoers.d/README 
+  102  sudo cat /etc/sudoers
+  103  ls -al
+  104  which anaconda
+  105  ls -al /home/lukasz/anaconda3/bin/anaconda
+  106  cat /home/lukasz/anaconda3/bin/anaconda
+  107  anaconda
+  108  anaconda -h
+  109  anaconda show
+  110  anaconda show spec
+  111  sudo anaconda show spec
+  112  man anaconda
+  113  anaconda -h | less
+  114  cal
+  115  wget https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
+  116  bash Anaconda3-4.3.1-Linux-x86_64.sh
+  117  anaconda
+  118  anaconda -h
+  119  bash Anaconda3-4.3.1-Linux-x86_64.sh
+  120  anaconda
+  121  cd /home/lukasz/anaconda3
+  122  ls
+  123  ls -l
+  124  cd/
+  125  cd /
+  126  bash Anaconda3-4.3.1-Linux-x86_64.sh
+  127  wget https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
+  128  bash Anaconda3-4.3.1-Linux-x86_64.sh
+  129  wget https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
+  130  bash Anaconda3-4.3.1-Linux-x86_64.sh
+  131  wget https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
+  132  bash Anaconda3-4.3.1-Linux-x86_64.sh
+  133  Anaconda3-4.3.1-Linux-x86_64.sh
+  134  wget https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
+  135  bash Anaconda3-4.3.1-Linux-x86_64.sh
+  136  yupyter
+  137  jupyter
+  138  jupyter -h
+  139  anaconda navigator
+  140  jupyter --runtime
+  141  jupyter --runtime-dir
+  142  runtime
+  143  anaconda -help
+  144  anaconda -h
+  145  anaconda navigator -h
+  146  sypder
+  147  spyder
+  148  jupyter
+  149  jupyter subcommand
+  150  jupyter
+  151  jupyter --version
+  152  jupyter4
+  153  jupyter-core
+  154  jupyter 4
+  155  jupyter --paths
+  156  anaconda3
+  157  cat $PATH
+  158  python
+  159  anaconda3
+  160  anaconda
+  161  python
+  162  anaconda 4
+  163  anaconda4
+  164  anaconda-navigator 
+  165  ls
+  166  cd anaconda3
+  167  ls
+  168  pwd
+  169  ls
+  170  cd Desktop
+  171  ls
+  172  ls -al
+  173  cd..
+  174  cd ..
+  175  ls
+  176  cd /
+  177  ls
+  178  cd bin
+  179  ls
+  180  pwd
+  181  cd ..
+  182  pwd
+  183  cd /
+  184  ls
+  185  cd bin
+  186  ls
+  187  cd /
+  188  pwd
+  189  ls
+  190  cd bin
+  191  ls
+  192  cd ..
+  193  ls
+  194  cd cbin
+  195  cs sbin
+  196  ls
+  197  cd sbin
+  198  cd /
+  199  cd tmp
+  200  ls
+  201  mkdir katalog1
+  202  cd katalog1
+  203  pwd
+  204  mkdir katalog2
+  205  cd katalog2
+  206  pwd
+  207  ls -a
+  208  ls -al
+  209  ls -l
+  210  ls -al
+  211  man ls
+  212  touch .ukryty
+  213  ls
+  214  la -a
+  215  ls -al
+  216  rm .ukryty
+  217  mkdir katalog3
+  218  rm katalog3
+  219  rmdir katalog3
+  220  ls
+  221  mkdir .ukryty_folder
+  222  ls -a
+  223  ls
+  224  ls -a
+  225  touch plik_testowy.txt
+  226  ls
+  227  echo "ING data science is cool"
+  228  echo "ING data science is cool"> plik_testowy.txt
+  229  cat plik_testowy.txt
+  230  echo "ING data science is very cool"> plik_testowy.txt
+  231  cat plik_testowy.txt
+  232  echo "ING data science is very cool">> plik_testowy.txt
+  233  cat plik_testowy.txt
+  234  echo "ING data science is very cool">> plik_testowy.txt
+  235  cat plik_testowy.txt
+  236  echo "ING data science is very cool">> plik_testowy.txt
+  237  echo "ING data science is preaty cool">> plik_testowy.txt
+  238  cat plik_testowy.txt
+  239  head plik_testowy.txt
+  240  tail plik_testowy.txt
+  241  head -5 plik_testowy.txt
+  242  cat plik_testowy.txt I head
+  243  cat plik_testowy.txt | head
+  244  cat plik_testowy.txt | -5 head | tail -2
+  245  cat plik_testowy.txt | -2 head | tail -2
+  246  cat plik_testowy.txt |  head -2 | tail -2
+  247  cat plik_testowy.txt | wc
+  248  cat plik_testowy.txt | wc -l
+  249  echo "ING data science is preaty cool">> plik_testowy.txt
+  250  echo "ING data science is ok">> plik_testowy.txt
+  251  ls --help
+  252  cat plik_testowy.txt | grep Ing
+  253  cat plik_testowy.txt
+  254  cat plik_testowy.txt | grep ING
+  255  man grep
+  256  cat plik_testowy.txt | grep -i ing
+  257  cat plik_testowy.txt | grep -i preaty cool
+  258  cat plik_testowy.txt | grep -i "preaty cool"
+  259  who
+  260  whoami
+  261  apt-get install postgres
+  262  sudo apt-get install postgres
+  263  sudo apt-cache search postgres
+  264  apt-get install postgressql
+  265  sudo apt-get install postgresql
+  266  env
+  267  env | user
+  268  env |grep -i user
+  269  echo $PATH
+  270  echo $USER
+  271  echo $PATH
+  272  ps
+  273  ps -a
+  274  pa -au
+  275  ps -au
+  276  ps -aux | grep firefox
+  277  kill 8225
+  278  vim
+  279  sudo apt install vim
+  280  vim
+  281  fg
+  282  mkdir nasze_dane
+  283  cd nasze_dane
+  284  touch imiona
+  285  echo "Lukasz" >> imiona
+  286  echo "Karolina" >> imiona
+  287  cat imiona
+  288  echo "Ania" >> imiona
+  289  echo "Asia" >> imiona
+  290  echo "Ewelina" >> imiona
+  291  echo "Pawel" >> imiona
+  292  cat imiona
+  293  sort --help
+  294  sort imiona
+  295  uniq imiona
+  296  wc -l
+  297  cat imiona wc -l
+  298  cat imiona | wc -l
+  299  /var/log/syslog
+  300  cd /
+  301  ls
+  302  cd var
+  303  cd log
+  304  cat syslog
+  305  cat syslog head -5
+  306  cat syslog | head -5
+  307  cat syslog | tail -5
+  308  cat syslog | grep -i error
+  309  cat syslog | grep -i error | wc -l
+  310  search error
+  311  grep -i error
+  312  cat syslog | grep -i error | wc -w
+  313  wc --help
+  314  cat syslog | grep -i error | wc -w
+  315  cat syslog | grep -i error
+  316  cat syslog | grep -i error | wc -w
+  317  cat syslog | grep -i error
+  318  cat syslog | wc -w | grep -i error
+  319  cat syslog | grep -i error
+  320  cat syslog | grep -i error | wc -w
+  321  syslog | grep -i error | wc -w
+  322  cat syslog | grep -i error | wc -w
+  323  cat syslog | wc error
+  324  wc -w syslog
+  325  cat syslog | wc -w -i error
+  326  cat syslog | wc -w error
+  327  cat syslog | grep -i error
+  328  cat syslog | grep -i error | wc "error"
+  329  cat syslog | grep -i error | wc - "error"
+  330  cat syslog | wc -i error 
+  331  cat syslog | wc -w error 
+  332  cd ~/
+  333  pwd
+  334  cd code/
+  335  cd code
+  336  pwd
+  337  mkdir DS_sages_Wwa
+  338  cd DS_sages_Wwa
+  339  ls
+  340  echo "# DS_sages_Wwa" >> README.md
+  341  ls
+  342  git init
+  343  sudo apt install git
+  344  git init
+  345  anaconda-navigator
+  346  ls
+  347  git clone https://github.com/LuksPiat/DS_sages_Wwa.git
+  348  cd DS_Sages_Wwa
+  349  ls
+  350  cd DS_sages_Wwa
+  351  ls
+  352  git status
+  353  git add .
+  354  git status
+  355  git commit -m "Update"
+  356  git push origin maaster
+  357  git push origin master
+  358  git status
+  359  https://github.com/pazikkamil/sages_271018.git
+  360  cd ..
+  361  cd tmp
+  362  ls
+  363  https://github.com/pazikkamil/sages_271018.git
+  364  cd ..
+  365  cd ~
+  366  pwd
+  367  cd tmp
+  368  git clone https://github.com/pazikkamil/sages_271018
+  369  ls
+  370  cd sages_271018
+  371  ls
+  372  git branch
+  373  git branch -a
+  374  git checkout dev
+  375  git branch -a
+  376  git branch
+  377  ls
+  378  git checkout master
+  379  ls
+  380  git branch
+  381  ls
+  382  git checkout -b "glupoty"
+  383  git branch
+  384  git branch
+  385  git checkout
+  386  git branch
+  387  git checkout master
+  388  git branch
+  389  git status
+  390  git checkout glupoty
+  391  git status
+  392  git branch
+  393  git status
+  394  touch nowy.txt
+  395  git status
+  396  git add nowy.txt
+  397  git status
+  398  git commit -m "nowy plik"
+  399  git push origin glupoty
+  400  git remote -v
+  401  jupyter-notebook
+  402  git branch
+  403  git checkout dev
+  404  git branch
+  405  jupyter-notebook
+  406  ls
+  407  pwd
+  408  ls
+  409  mkdir code
+  410  cd code
+  411  mkdir sages_oop
+  412  cd sages_oop
+  413  echo "# sages_oop" >> README.md
+  414  ls
+  415  git init
+  416  ls -al
+  417  git status
+  418  git add README.md
+  419  git status
+  420  git commit -m "first commit"
+  421  git status
+  422  git remote add origin https://github.com/LuksPiat/sages_oop.git
+  423  git remote -v
+  424  git push -u origin master
+  425  jupyter-notebook
+  426  git status
+  427  sudo add-apt-repository ppa:serge-rider/dbeaver-ce
+  428  sudo apt-get update
+  429  sudo apt-get install dbeaver-ce
+  430  pwd
+  431  ls
+  432  cd code/
+  433  ls
+  434  cd ..
+  435  cd tmp/
+  436  git remote -v
+  437  ls
+  438  cd sages_271018/
+  439  ls
+  440  git remote -v
+  441  git branch
+  442  git pull origin master
+  443  git checkout -b rozwiazania_lukasz_piatkowski
+  444  git branch
+  445  ls
+  446  cd ..
+  447  cp code/sages_oop/OOP_1.ipynb tmp/sages_271018/OOP_rozwiazania.ipynb
+  448  cd tmp/sages_271018/
+  449  ls
+  450  git branch
+  451  git status
+  452  git add .
+  453  git commit -m "czesc mam prawie skonczone"
+  454  git push origin rozwiazania_lukasz_piatkowski
+  455  cd ~\
+  456  ls
+  457  cd DS_sages_Wwa
+  458  history > History2.bash
